4d47a425c3a5834d8379200595b1e93fe8d3c3a3
elvis.pysqyli <elvis.pysqyli@gmail.com>
2025-08-14 14:32:17 +0200

    example: rename asm to assembler in fill-screen

--- examples/fill-screen.rs
+++ examples/fill-screen.rs
@@ -4,7 +4,7 @@ use hack_assembler::assembler::Assembler;
 
 fn main() {
     match Assembler::from_file("examples/fill-screen.asm") {
-        Ok(asm) => match asm.compile() {
+        Ok(assembler) => match assembler.compile() {
             Ok(hack) => match fs::write("examples/fill-screen.hack", hack.join("\n")) {
                 Ok(_) => println!("fill-screen compiled to hack"),
                 Err(err) => eprintln!("{err}"),
